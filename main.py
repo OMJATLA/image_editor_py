1458872bbdd3de73d19ecce266fc6d157da6b4f1
Abhishek Bajpai <93428862+Talisman-407@users.noreply.github.com>
2025-01-31 02:44:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,54 +6,40 @@ from io import BytesIO
 from fastapi.responses import Response
 
 server = FastAPI()
-
-API_TOKEN = "0784c37334e44f509b4343f27d90de40"
-
+BRIA_API_TOKEN = "0784c37334e44f509b4343f27d90de40"
 REMOVE_BG_URL = "https://engine.prod.bria-api.com/v1/background/remove"
-# REPLACE_BG_URL = "https://engine.prod.bria-api.com/v1/background/replace"
-
+GENERATE_BG_URL = "https://engine.prod.bria-api.com/v1/background/replace"
 
 @server.get("/test")
 async def test_server():
     return {"message": "Server is running successfully!"}
 
-@server.post("https://img.api.stage.shoopy.in/background/remove")
+@server.post("/background/remove")
 async def remove_background(request: Request):
         image_data = await request.body()
-
         result = call_bria_api(REMOVE_BG_URL, files={"file": image_data})
-
         return result
-
 
 # class ImageRequest(BaseModel):
 #     prompt: str
 #     image: str 
 
-
-# @server.post("/generate-background")
-# async def generate_image(request_data: ImageRequest):
-
-#     image_file =request_data.image
-#     result = call_bria_api(REPLACE_BG_URL, data={"bg_prompt": request_data.prompt,"file": image_file})
-
-#     return result
-
-
+@server.post("/background/generate")
+async def generate_image(request_data: ImageRequest):
+    image_file =request_data.image
+    result = call_bria_api(REPLACE_BG_URL, data={"bg_prompt": request_data.prompt,"file": image_file})
+    return result
 
 def call_bria_api(url: str, files=None, data=None, json=None):
     headers = {
-        "api_token": API_TOKEN}
-    
+        "api_token": BRIA_API_TOKEN}
     try:
         response = requests.post(url, headers=headers, data=data,files=files,json=json)
-        
         print(response)
         if response.status_code == 200:
             return response.json()
         else:
             return 'Failed to process image'
-    
     except Exception as e:
         return str(e)
     
